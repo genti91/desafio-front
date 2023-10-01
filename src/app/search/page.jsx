75a0f05f3e6a9691a9bc9b7fe4b19d080c125c5d
genti91 <franco.g7@hotmail.com>
2023-10-01 19:30:46 -0300

    undo search refactor

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,8 +1,31 @@
-import SearchedMovies from "./SearchedMovies";
+import ContentCard from '@/components/ContentCard'
 
 export default async function Search({searchParams}) {
-
+  let data = await getData(searchParams.s)
   return (
-    <SearchedMovies searchParams={searchParams} />
+    <div className="h-[90%] flex justify-center py-10">
+      {data.length === 0 ? <div className='text-3xl'>No movies found</div>
+      :
+      <div className='flex flex-wrap gap-10 w-[80%] justify-center'>
+        {data.map((movie, index) => {
+          return (
+            <div key={index} className='h-[150px] w-80'>
+              <ContentCard section='search' item={movie} key={index} />
+            </div>
+          )
+        })}
+      </div>
+      }
+    </div>
   )
+}
+
+const getData = async (search) => {
+  try {
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/search/?s='+search, {cache: "no-store"}).then(res => res.json())
+    return res.movies
+  } catch (err) {
+    console.log(err)
+    return []
+  }
 }
