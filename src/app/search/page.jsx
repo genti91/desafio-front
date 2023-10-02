d6a1f4a3b55922d2f5ad28432a72b925aa7da398
genti91 <franco.g7@hotmail.com>
2023-10-01 22:25:31 -0300

    fix: deploy search error

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,24 +1,24 @@
 import ContentCard from '@/components/ContentCard'
-import Image from "next/image"
-import allMovies from "../../assets/allMovies.svg"
+import Image from 'next/image'
+import allMovies from '../../assets/allMovies.svg'
 import Link from 'next/link'
 
 export default async function Search({searchParams}) {
   let data = await getData(searchParams.s)
   return (
-    <div className="flex flex-col gap-5 h-[90%] 2xl:w-[72vw] w-full mx-auto py-10 2xl:px-auto xs:px-16 px-3">
-      <div className="flex justify-between items-center">
-        <p className="font-bold text-lg">Search results for "{searchParams.s}"</p>
+    <div className='flex flex-col gap-5 h-[90%] 2xl:w-[72vw] w-full mx-auto py-10 2xl:px-auto xs:px-16 px-3'>
+      <div className='flex justify-between items-center'>
+        <p className='font-bold text-lg'>Search results for "{searchParams.s}"</p>
         <Link href='/'>
-          <div className="flex gap-[10px] items-center cursor-pointer">
-            <p className="text-[12px] text-[#606265] font-medium">Go Back</p>
-            <Image src={allMovies} alt="All movies" />
+          <div className='flex gap-[10px] items-center cursor-pointer'>
+            <p className='text-[12px] text-[#606265] font-medium'>Go Back</p>
+            <Image src={allMovies} alt='All movies' />
           </div>
         </Link>
       </div>
-      <div className="flex gap-8 flex-wrap overflow-y-scroll h-full justify-center">
+      <div className='flex gap-8 flex-wrap overflow-y-scroll h-full justify-center'>
         {data.map((movie, i) => (
-          <div key={i} className={i === 2 ? "h-[190px] lg:block hidden w-96" : "h-[190px] w-96"}>
+          <div key={i} className={i === 2 ? 'h-[190px] lg:block hidden w-96' : 'h-[190px] w-96'}>
             <ContentCard section='search' item={movie} />
           </div>
         ))}
@@ -30,7 +30,7 @@ export default async function Search({searchParams}) {
 
 const getData = async (search) => {
   try {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/search/?s='+search, {cache: "no-store"}).then(res => res.json())
+    const res = await fetch(process.env.NEXT_PUBLIC_API_URL+'/api/search/?s='+search, {cache: 'no-store'}).then(res => res.json())
     return res.movies
   } catch (err) {
     console.log(err)
